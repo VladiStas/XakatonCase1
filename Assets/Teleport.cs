9732dace7f3fca76ee18dc95b17aa906aa78efab
living-the-future <91959331+living-the-future@users.noreply.github.com>
2022-06-30 15:21:33 +0300

    Day 4. Fixed the Teleport.

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -5,86 +5,70 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public GameObject[] objs;
+    public float time=0f;
     public GameObject image;
     public float distance;
+    float player_distance;
     RaycastHit hit;
     private void Update()
     {
-       
+        TeleportPlayer(objs);
+    }
+
+    void ActivePoint(GameObject[] gm)
+    {
+        for (int i = 0; i < gm.Length; i++)
+            gm[i].SetActive(true);
+    }
+    void TeleportPlayer(GameObject[] gm, RaycastHit hit, int i, float distance)
+    {
+
+      
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position = gm[i].transform.position;
+            gm[i].active = false;
+            
+            image.SetActive(false);
+        }
+        
+            
+      
+    }
+
+    void TeleportPlayer(GameObject[] gm)
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Physics.Raycast(ray, out hit);
         Debug.DrawRay(transform.position, ray.direction * distance, Color.yellow);
-
-        if(hit.collider != null)
+        time += Time.deltaTime;
+        Debug.Log($"Time:{time}");
+        if (hit.collider != null)
         {
+            for (int i = 0; i < objs.Length; i++)
+            {
 
-            TeleportPlayer(); 
-            
+                player_distance = Vector3.Distance(gm[i].transform.position, transform.position);
+                if (hit.collider.gameObject == gm[i].gameObject)
+                {
+
+                    image.SetActive(true);
+                    TeleportPlayer(objs, hit, i, player_distance);
+
+                    if (time> 1f)
+                    {
+                        ActivePoint(gm);
+                        time = 0f;
+                    }
+
+                    break;
+                }
+                else { image.SetActive(false); }
+
+            }
+
         }
-        else { image.SetActive(false); }
-
+        else { image.SetActive(false); time = 0; }
     }
-
-    void TeleportPlayer()
-    {
-
-        if (hit.collider.gameObject == objs[0].gameObject)
-        {
-            image.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                transform.position = objs[0].transform.position;
-                image.SetActive(false);
-            }
-        }
-        else if (hit.collider.gameObject == objs[1].gameObject)
-        {
-            image.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                transform.position = objs[1].transform.position;
-                image.SetActive(false);
-            }
-        }
-        else if (hit.collider.gameObject == objs[2].gameObject)
-        {
-            image.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                transform.position = objs[2].transform.position;
-                image.SetActive(false);
-            }
-        }
-        else if (hit.collider.gameObject == objs[3].gameObject)
-        {
-            image.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                transform.position = objs[3].transform.position;
-                image.SetActive(false);
-            }
-        }
-        else if (hit.collider.gameObject == objs[4].gameObject)
-        {
-            image.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                transform.position = objs[4].transform.position;
-                image.SetActive(false);
-            }
-        }
-        else if (hit.collider.gameObject == objs[5].gameObject)
-        {
-            image.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                transform.position = objs[5].transform.position;
-                image.SetActive(false);
-            }
-        }
-        else image.SetActive(false);
-       
-    }
-   
 
 }
